bcefe887cfc073c32b1ddfc8634302f2abbf3896
xjz <13456722654@139.com>
2021-08-09 14:17:46 +0800

    add transfer

--- fetch-data/index.js
+++ fetch-data/index.js
@@ -1,5 +1,6 @@
 // Import
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+const { ApiPromise, Keyring, WsProvider } = require('@polkadot/api');
+const { UnsubscriptionError } = require('rxjs');
 
 wsAddr = "ws://127.0.0.1:9944"
 
@@ -10,7 +11,43 @@ async function main() {
     const api = await ApiPromise.create({ provider: wsProvider });
 
     // Do something
+    //===================================
     console.log(api.genesisHash.toHex());
+    // Retrieve the chain name
+    const chain = await api.rpc.system.chain();
+
+    // Retrieve the latest header
+    const lastHeader = await api.rpc.chain.getHeader();
+
+    // Log the information
+    console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
+    
+    //transfer test
+    const keyring = new Keyring({ type: 'sr25519' });
+    const aliceAccount = keyring.addFromUri('//Alice');
+    const bobAccount = keyring.addFromUri('//Bob');
+
+    console.log(`alice:${aliceAccount.address}`);
+    console.log(`bob:${bobAccount.address}`);
+    console.log("transfer...")
+
+    const txHash = await api.tx.balances
+    .transfer(bobAccount.address, 1000000000000000)
+    .signAndSend(aliceAccount, ({ events = [], status}) => {
+        console.log(`current status is ${status.type}`);
+
+        if (status.isFinalized) {
+            console.log(`transaction included at blockHash ${status.asFinalized}`);
+
+            events.forEach(({phase, event: {data, method, section}}) => {
+                console.log(`\t ${phase}: ${section}.${method}:: ${data}`);
+            });
+            //unsub();
+        }
+    });
+
+    console.log(`submit with hash ${txHash}`)
+    //====================================
 }
 
 main().catch(console.error)
