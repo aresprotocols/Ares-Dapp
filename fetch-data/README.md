2d742b3755bab4864cf312d929b8a91280276918
xjz <13456722654@139.com>
2021-08-19 16:26:36 +0800

    add fetch-data

--- fetch-data/README.md
+++ fetch-data/README.md
@@ -1,4 +1,9 @@
+# scanner
+Query a well-known event（aresModule:OracleRequest）and extract information from them， sends the request to the Aggregator and  provide back the result.
+
+# Running scanner
 ```bash
-yarn install
+npm install
 node index.js
 ```
+
--- fetch-data/index.js
+++ fetch-data/index.js
@@ -1,14 +1,90 @@
-// Import
 const { ApiPromise, Keyring, WsProvider } = require('@polkadot/api');
-const { UnsubscriptionError } = require('rxjs');
+const { cryptoWaitReady }  = require('@polkadot/util-crypto');
+const { hexToBn, u8aToBuffer, bufferToU8a } = require('@polkadot/util')
 
-wsAddr = "ws://127.0.0.1:9944"
+const PHRASE = 'entire material egg meadow latin bargain dutch coral blood melt acoustic thought';
+
+async function fundAggregatorAccountIfNeeded(api, aliceAccount, aggregatorAccount) {
+  return new Promise(async (resolve) => {
+    console.log("aggregator 1")
+    let { data: { free: previousFree }, nonce: previousNonce } = await api.query.system.account(aggregatorAccount.address);
+    console.log("aggregator 2", previousFree)
+    if (previousFree.isZero()) {
+        console.log("previous zero 1")
+        await api.tx.balances.transfer(aggregatorAccount.address, 1000000000000000).signAndSend(aliceAccount, async ({ status }) => {
+          console.log("after transfer 1")
+          if (status.isFinalized) {
+            resolve();
+          }
+        });
+        console.log('Operator funded');
+    } else {
+      console.log("aggregator 3")
+      resolve();
+    }
+  });
+}
+
+async function registerAggregatorIfNeeded(api, aggregatorAccount) {
+  // Register the aggregator, this is supposed to be initiated once by the aggregator itself
+  return new Promise(async (resolve) => {
+    const operator = await api.query.aresModule.aggregators(aggregatorAccount.address);
+      console.log('Operator register'," ",operator.toJSON()," ",aggregatorAccount.address);
+      if (operator.toJSON().block_number === 0) {
+          console.log("before register aggregator")
+          await api.tx.aresModule.registerAggregator('ok,huobi','jack', 'http://141.164.45.97:8080/ares/api').signAndSend(aggregatorAccount, async ({ status }) => {
+            console.log('Operator registered',status.isFinalized);
+            if (status.isFinalized) {
+            console.log('Operator registered');
+            resolve();
+          }
+        });
+    } else {
+      resolve();
+    }
+  });
+}
+
+function sentRes(url,data,method,fn){
+  data=data||null;
+  if(data==null){
+      var content=require('querystring').stringify(data);
+  }else{
+      var content = JSON.stringify(data); //json format
+  }
+
+  var parse_u=require('url').parse(url,true);
+  var isHttp=parse_u.protocol=='http:';
+  var options={
+      host:parse_u.hostname,
+      port:parse_u.port||(isHttp?80:443),
+      path:parse_u.path,
+      method:method,
+      headers:{
+          'Content-Type':'application/json',
+          'Content-Length':Buffer.byteLength(content,"utf8"),
+          'Tracking-Api-Key':'YOUR API KEY'
+      }
+  };
+  var req = require(isHttp?'http':'https').request(options,function(res){
+      var _data='';
+      res.on('data', function(chunk){
+          _data += chunk;
+      });
+      res.on('end', function(){
+          fn!=undefined && fn(_data);
+      });
+  });
+  req.write(content);
+  req.end();
+}
 
 async function main() {
-    console.log("main enter, before connect to ws")
-    // Construct
-    const wsProvider = new WsProvider(wsAddr);
-    const api = await ApiPromise.create({ 
+    await cryptoWaitReady();
+
+    // Connect to the local chain
+    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+    const api = await ApiPromise.create({
         provider: wsProvider,
         types: {
             "TokenSpec": "Vec<u8>",
@@ -32,44 +108,90 @@ async function main() {
         }
     });
 
-    // Do something
-    //===================================
-    console.log(api.genesisHash.toHex());
-    // Retrieve the chain name
-    const chain = await api.rpc.system.chain();
+    const Bytes2HexString =(arr)=> {
+        for(j = 0; j < arr.length; j++) {
+        if(arr[j].indexOf("TokenSpec")>-1)
+        {
+          var str=arr[j].split(":")[1].trim()
 
-    // Retrieve the latest header
-    const lastHeader = await api.rpc.chain.getHeader();
+          var curCharCode;
+        　　var resultStr = [];
 
-    // Log the information
-    console.log(`${chain}: last block #${lastHeader.number} has hash ${lastHeader.hash}`);
-    
-    //transfer test
-    const keyring = new Keyring({ type: 'sr25519' });
-    const aliceAccount = keyring.addFromUri('//Alice');
-    const bobAccount = keyring.addFromUri('//Bob');
+        　　for(var i = 2; i < str.length;i = i + 2) {
+        　　　　curCharCode = parseInt(str.substr(i, 2), 16); // ASCII Code Value
+        　　　　resultStr.push(String.fromCharCode(curCharCode));
+        　　}
+        　　return resultStr.join("");
 
-    console.log(`alice:${aliceAccount.address}`);
-    console.log(`bob:${bobAccount.address}`);
-    console.log("transfer...")
-
-    const txHash = await api.tx.balances
-    .transfer(bobAccount.address, 1000000000000000)
-    .signAndSend(aliceAccount, ({ events = [], status}) => {
-        console.log(`current status is ${status.type}`);
-
-        if (status.isFinalized) {
-            console.log(`transaction included at blockHash ${status.asFinalized}`);
-
-            events.forEach(({phase, event: {data, method, section}}) => {
-                console.log(`\t ${phase}: ${section}.${method}:: ${data}`);
-            });
-            //unsub();
         }
+      }
+      return "error";
+    }
+
+    // Add an account, straight from mnemonic
+    const keyring = new Keyring({ type: 'sr25519' });
+    const aggregatorAccount = keyring.addFromUri(PHRASE);
+    console.log(`Imported aggregator with address ${aggregatorAccount.address}`);
+
+    // Make sure this aggregator has some funds
+    const aliceAccount = keyring.addFromUri('//Alice');
+    console.log(`alice ${aliceAccount.address}`);
+    console.log("before start fundAggregator")
+    await fundAggregatorAccountIfNeeded(api, aliceAccount, aggregatorAccount);
+    console.log("after fundAggregator")
+    const result = await api.query.aresModule.oracleResults("btcusdt");
+    console.log("after oracle results", result)
+    // Listen for ares.OracleRequest events
+    api.query.system.events((events) => {
+        events.forEach(record => {
+          // extract the phase, event and the event types
+          const { event, phase} = record;
+          const types = event.typeDef;
+
+          // show what we are busy with
+          const eventName = `${event.section}:${
+            event.method
+          }:: (phase=${phase.toString()})`;
+
+          if (event.section == "aresModule" && event.method == "OracleRequest") {
+              // loop through each of the parameters, displaying the type and data
+              const params = event.data.map(
+                (data, index) => `${types[index].type}: ${data.toString()}`
+              );
+              console.log(`${params}`);
+              //console.log(eventName);
+
+              const id = event.data[2].toString();
+              console.log(id);
+              var postData = null;
+              var SpecIndex = Bytes2HexString(params)
+              //get aggregator url and loading
+              //var url =`http://141.164.45.97:8080/ares/api/getPartyPrice/${SpecIndex}/${id}`;
+              var url =`http://127.0.0.1:5566/api/getPartyPrice/${SpecIndex}`;
+              console.log(url)
+              sentRes(url,postData,"GET",function(data) {
+                console.log(JSON.parse(data).data)
+                var price=JSON.parse(data).data.price * 1000;
+                const result = api.createType('i128', price).toHex();
+                // Respond to the request with a dummy result
+                api.tx.aresModule.feedResult(parseInt(id), result).signAndSend(aggregatorAccount, async ({ events = [], status }) => {
+                    if (status.isFinalized) {
+                        const updatedResult = await api.query.aresModule.oracleResults(SpecIndex);
+                        console.log(`${SpecIndex} price is now ${updatedResult[updatedResult.length -1]/1000}`);
+                    }
+                  });
+                  console.log(data);
+               });
+          }
+        
+      });
     });
 
-    console.log(`submit with hash ${txHash}`)
-    //====================================
+    await registerAggregatorIfNeeded(api, aggregatorAccount);
+
+    // Then simulate a call from alice
+    await api.tx.aresModule.initiateRequest(aggregatorAccount.address, "btcusdt", "1").signAndSend(aliceAccount);
+    console.log(`Alice send a request`);
 }
 
 main().catch(console.error)
